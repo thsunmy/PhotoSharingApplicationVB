09119e4ad57faf217340f85255e9c73a7bb3dc04
thsun <thsun@live.com.my>
2015-08-07 01:08:40 +0800

    Connection String and Seeding the database
    Connection String and Seeding the database

--- Global.asax.vb
+++ Global.asax.vb
@@ -1,9 +1,15 @@
 ﻿Imports System.Web.Optimization
+Imports System.Data.Entity
 
 Public Class MvcApplication
     Inherits System.Web.HttpApplication
 
     Protected Sub Application_Start()
+
+        'Seed the database with sample data for development. This code should be removed for production.
+        Database.SetInitializer(Of PhotoSharingContext)(New PhotoSharingInitializer())
+
+
         AreaRegistration.RegisterAllAreas()
         FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters)
         RouteConfig.RegisterRoutes(RouteTable.Routes)
